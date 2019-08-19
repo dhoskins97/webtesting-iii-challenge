84b6613bd0c6d0fceb404b88564b9eecf955d44b
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-08-19 19:06:21 -0400

    added additional tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
-// Test away
+// Test away!
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+afterEach(cleanup);
+
+describe('Dashboard.js', () => {
+    it("renders without crashing", () => {
+        render(<Dashboard />)
+    })
+
+    // it("cannot be opened or closed if locked",() => {
+    //     const displayLocked = render(<Display locked={true} />);
+    //     displayLocked.fireEvent.clicked.getByText(//i)
+    // })
+    
+});
